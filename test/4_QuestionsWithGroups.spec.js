d81d4ac893d98dad6ceac28c9de464fb4cb9dd37
pavkahanov <pavkahanov@neos1.com>
2020-01-28 18:01:01 +0700

    Tests for QuestionsWithGroups

--- test/4_QuestionsWithGroups.spec.js
+++ test/4_QuestionsWithGroups.spec.js
@@ -1,4 +1,5 @@
 const QuestionsWithGroups = artifacts.require('./QuestionsWithGroups.sol');
+const getErrorMessage = require('./helpers/get-error-message');
 
 contract('QuestionsWithGroups', (accounts) => {
     let questionsWithGroups;
@@ -33,8 +34,90 @@ contract('QuestionsWithGroups', (accounts) => {
         });
     });
 
+    describe('addQuestion()', () => {
+        it('should add question', async () => {
+            await questionsWithGroups.addQuestion(question);
+            const uploaded = await questionsWithGroups.getQuestion(0);
+            assert.strictEqual(uploaded.name, 'question name');
+        });
+
+        it('shoud fail on uploading two same questions', async () => {
+            try {
+                const questionDuplicate = Object.create(question);
+                await questionsWithGroups.addQuestion(question);
+                await questionsWithGroups.addQuestion(questionDuplicate);
+            } catch (e) {
+                assert.strictEqual(e.message, getErrorMessage('Name must be unique'))
+            } 
+        });
+    });
+
+    describe('getQuestionGroupsAmount()', () => {
+        it('should successful check amount of groups', async () => {
+            const group  = { name: 'something' };
+            await questionsWithGroups.addQuestionGroup(group);
+            const countOfUploaded = await questionsWithGroups.getQuestionGroupsAmount();
+            assert.equal(countOfUploaded, web3.utils.toBN(2));
+        });
+       
+    });
+
+    describe('getQuestionGroup()', () => {
+        it('should successful get group "system"', async () => {
+            const group = await questionsWithGroups.getQuestionGroup(0);
+            assert.equal(group.name, 'system');
+        });
+
+        it('should fail on getting group out of bounds', async () => {
+            try {
+                await questionsWithGroups.getQuestionGroup(25);
+            } catch (e) {
+                assert.equal(e.message, 'Returned error: VM Exception while processing transaction: revert Provided index is out of bounds');
+            }
+        });
+    });
+
+    describe('addQuestionGroup()', () => {
+        it('should successful add group', async () => {
+            await questionsWithGroups.addQuestionGroup(group);
+            const uploadedGroup = await questionsWithGroups.getQuestionGroup(1);
+            assert.strictEqual(uploadedGroup.name , 'group name');
+        });
+
+        it('should fail on non-unique name', async () => {
+            try {
+                await questionsWithGroups.addQuestionGroup(group);
+                await questionsWithGroups.addQuestionGroup(group);
+            } catch (e) {
+                assert.strictEqual(e.message, getErrorMessage('Name must be unique'))
+            }
+        });
+    });
+
+    describe('setQuestionGroupName()', () => {
+        it('should successful change group name', async () => {
+            name = 'test'
+            await questionsWithGroups.addQuestionGroup(group);
+            await questionsWithGroups.setQuestionGroupName(1, name);
+            const uploadedGroup = await questionsWithGroups.getQuestionGroup(1);
+            assert.strictEqual(uploadedGroup.name, name);
+        });
+
+        it('should fail on change name of non-unique group', async ()=> {
+            try {
+                name = 'test'
+                await questionsWithGroups.setQuestionGroupName(12, name);
+            } catch(e) {
+                assert.strictEqual(e.message, getErrorMessage('Provided index is out of bounds'));
+            }
+        })
+    });
+
+    
+
+
     // TODO: write tests for other cases
-    // 1. test non-unique names
+    // * 1. test non-unique names
     // 2. test upload question and group with same names 
     // 3. test group name update
     // after user groups, owner functionality are implemented
