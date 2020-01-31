43fe643084200300b305b30fb4500ed1e5e95fb1
pavkahanov <pavkahanov@neos1.com>
2020-01-31 14:16:31 +0700

    Controlled contract tests

--- test/6_Controlled.spec.js
+++ test/6_Controlled.spec.js
@@ -32,11 +32,44 @@ contract('ZeroOne', (accounts) => {
     beforeEach(async () => {
         zeroOne = await ZeroOne.new({ from: deployFrom });
         controlled = await Controlled.new(zeroOne.address, { from: deployFrom });
+        //console.log(controlledMethods);
     });
 
     describe('events', () => {
         it('should fire ZeroOneCall event after successful call', async () => {
-            // TODO: test event
+            const {address} = controlled;
+            const {testSuccess: {selector}} = controlledMethods;
+
+            await zeroOne.testMakeCall(address, selector, data, metaData);
+
+            const [event] = await controlled.getPastEvents("ZeroOneCall");
+            const {ballotId, questionId, startBlock, endBlock, result} = event.args.meta;
+            assert.strictEqual(Number(ballotId), metaData.ballotId);
+            assert.strictEqual(Number(questionId), metaData.questionId);
+            assert.strictEqual(Number(startBlock), metaData.startBlock);
+            assert.strictEqual(Number(endBlock), metaData.endBlock);
+            assert.strictEqual(Number(result), metaData.result);
+        });
+
+        it('should not fire ZeroOneCall event after failed call', async () => {
+            const {address} = controlled;
+            const {testRevert: {selector}} = controlledMethods;
+
+            await zeroOne.testMakeCall(address, selector, data, metaData);
+
+            const events = await controlled.getPastEvents("ZeroOneCall");
+            assert.strictEqual(events.length, 0);
+        });
+
+        it('should revert with message', async () => {
+            const {address} = controlled;
+            const {testRevertMessage: {selector}} = controlledMethods;
+            const tx = await zeroOne.testMakeCall(address, selector, data, metaData);
+            const log = tx.logs.find(element => element.event.match('Call'));
+            const message = web3.utils.toAscii(log.args.response);
+            assert.strictEqual(log.args.selector, selector);
+            assert.strictEqual(log.args.result, false);
+            assert.strictEqual(message.includes('test'), true);
         });
     })
 });
