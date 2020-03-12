fa595af8fc3a1c5c2976678722b1ae1f2d4feb42
pavkahanov <pavkahanov@neos1.com>
2020-03-12 18:24:38 +0700

    new structure of tests

--- test/11_1_ZeroOne.spec.js
+++ test/11_1_ZeroOne.spec.js
@@ -18,143 +18,221 @@ contract('ZeroOne', ([from, secondary, third]) => {
 
 
 
-
-
-  const formulas = [
-    `erc20{%s}->conditions{quorum>30%, positive>50% of all} and (custom{%t}->conditions{quorum>30%, positive>50% of all} and custom{%t}->admin)`, 
-    `erc20{%s}->conditions{quorum>50%, positive>90% of quorum}`,
-    `erc20{%s}->conditions{quorum>50%, positive=100% of quorum}`,
-    `erc20{%s}->conditions{quorum>0%, positive>50% of all}`,
-    `erc20{%s}->conditions{quorum>50%,positive>90% of quorum} or custom{%t}->admin`,
-    `erc20{%s}->conditions{quorum>50%,positive>90% of quorum} and custom{%t}->admin`, 
-    `erc20{%s}->conditions{quorum>50%,positive>90% of quorum} and custom{%t}->admin`, 
-  ];
-
-  beforeEach(async () => {
-
-  });
-
-  describe('fullVotingProcess', async () => {
-    it("should finish voting with ACCEPTED result", async () => {
-      for (formula of formulas) {
-        token = await ERC20.new('test', 'tst', 1000);
-        customToken = await CustomToken.new('test', 'tst', 1000);
-
-        const zeroOneVm = await ZeroOneVM.new();
-        const ballotType =  await BallotType.new();
-        
-        await ZeroOne.link("ZeroOneVM", zeroOneVm.address);
-        await ZeroOne.link("BallotType", ballotType.address);
-
-        const group = {
-          name: "Owners",
-          groupAddress: token.address,
-          groupType: 0
-        }
-    
-        zeroOne = await ZeroOne.new(group, { from });
-        controlled = await Controlled.new(zeroOne.address, { from });
-        
-        customToken.addToProjects(zeroOne.address);
-
-        for (question of questions) {
-          question.rawFormula = formula;
-          question.formula = compile(
-            question.rawFormula
-            .replace(/\%s/g, token.address)
-            .replace(/\%t/g, customToken.address)
-            .replace(/\%u/g, from)
-            )
-            question.target = zeroOne.address;
-            await zeroOne.addQuestion(question);
-          }
-          
-        const adminBalance = await token.balanceOf(from);
-        const data = web3.eth.abi.encodeParameters(['tuple(uint256,uint256,uint256,uint256,uint256)', 'tuple(string)'],[[0, 0, 0, 0, 0], ["test"]])
-        const votingData = {
-          questionId: 2,
-          starterAddress: from,
-          starterGroupId: 0,
-          endTime: 0,
-          data,
-        }
-        
-        await zeroOne.startVoting(votingData);
-        try {
-          await token.approve(zeroOne.address, adminBalance);
-          await zeroOne.setVote(1);
-
-          increase(web3, 320000);
-          await zeroOne.submitVoting();
-          const [event] = await zeroOne.getPastEvents('VotingEnded');
-          const {args: {descision}} = event;
-          assert.strictEqual(descision.toNumber(), 1);
-          console.log(`voting with formula --- ${formula} --- ✔️`);
-        } catch ({message}) {
-          console.log(`test #${formulas.indexOf(formula)} ---  ${message} --- 🚫`)
-        }
+  const uploadQuestions = async (formula, zeroOne) => {
+    for (question of questions) {
+      question.rawFormula = formula;
+      question.formula = compile(
+        question.rawFormula
+        .replace(/\%s/g, token.address)
+        .replace(/\%t/g, customToken.address)
+        .replace(/\%u/g, from)
+        )
+        question.target = zeroOne.address;
+        await zeroOne.addQuestion(question);
       }
+  }
+
+  const startVoting = async (zeroOne, token) => {
+    const adminBalance = await token.balanceOf(from);
+    const data = web3.eth.abi.encodeParameters(['tuple(uint256,uint256,uint256,uint256,uint256)', 'tuple(string)'],[[0, 0, 0, 0, 0], ["test"]])
+    const votingData = {
+      questionId: 2,
+      starterAddress: from,
+      starterGroupId: 0,
+      endTime: 0,
+      data,
+    }
+    
+    await zeroOne.startVoting(votingData);
+    await token.approve(zeroOne.address, adminBalance);
+  }
+
+  const makeTransfers = async (token, transfers) => {
+
+  }
+
+  const makeVotes = async (zeroOne, votes) => {
+
+  }
+
+  const getDescision = async (zeroOne) => {
+    const [event] = await zeroOne.getPastEvents('VotingEnded');
+    const {args: {descision}} = event;
+    return descision.toNumber();
+  }
+
+  const Voter = async (formula, data) => {
+    token = await ERC20.new('test', 'tst', 1000);
+    customToken = await CustomToken.new('test', 'tst', 1000);
+
+    const group = {
+      name: "Owners",
+      groupAddress: token.address,
+      groupType: 0
+    }
+
+    const zeroOneVm = await ZeroOneVM.new();
+    const ballotType =  await BallotType.new();
+    
+    await ZeroOne.link("ZeroOneVM", zeroOneVm.address);
+    await ZeroOne.link("BallotType", ballotType.address);
+
+
+    zeroOne = await ZeroOne.new(group, { from });
+    controlled = await Controlled.new(zeroOne.address, { from });
+    
+    customToken.addToProjects(zeroOne.address);
+
+    await uploadQuestions(formula, zeroOne);
+
+    await startVoting(zeroOne, token);
+
+    await zeroOne.setVote(1);
+
+    await makeVotes(zeroOne, votes);
+
+    increase(web3, 320000);
+
+    await zeroOne.submitVoting();
+
+    return(await getDescision(zeroOne));
+  }
+
+  describe("erc20{%s}->conditions{quorum>50%, positive>90% of quorum}", () => {
+    it('should be positive', async () => {
+      let formula = `erc20{%s}->conditions{quorum>50%, positive>90% of quorum}`;
+      let descision;
+      let data = {
+        "transfers": {
+          "before":[{
+            "from": "",
+            "to": "",
+            "amount": ""
+          }],
+          
+          "after": [{
+            "from": "",
+            "to": "",
+            "amount": ""
+          }]
+        },
+      
+        "votes":[
+          {
+            "sender":"",
+            "vote":""
+          }
+        ],
+        "expected":""
+      }
+      
+      try {
+        descision = await Voter(formula);
+      } catch({ message }) {
+        console.log(message)
+      }
+      assert.strictEqual(descision, 1);  
     });
 
-    it('should finish voting with DECLINED result', async () => {
-      for (formula of formulas) {
-        token = await ERC20.new('test', 'tst', 1000);
-        customToken = await CustomToken.new('test', 'tst', 1000);
+    it('should be negative', async () => {});
 
-        const zeroOneVm = await ZeroOneVM.new();
-        const ballotType =  await BallotType.new();
-        
-        await ZeroOne.link("ZeroOneVM", zeroOneVm.address);
-        await ZeroOne.link("BallotType", ballotType.address);
-
-        const group = {
-          name: "Owners",
-          groupAddress: token.address,
-          groupType: 0
-        }
-    
-        zeroOne = await ZeroOne.new(group, { from });
-        controlled = await Controlled.new(zeroOne.address, { from });
-        
-        customToken.addToProjects(zeroOne.address);
-
-        for (question of questions) {
-          question.rawFormula = formula;
-          question.formula = compile(
-            question.rawFormula
-            .replace(/\%s/g, token.address)
-            .replace(/\%t/g, customToken.address)
-            .replace(/\%u/g, from)
-            )
-            question.target = zeroOne.address;
-            await zeroOne.addQuestion(question);
-          }
-          
-        const adminBalance = await token.balanceOf(from);
-        const data = web3.eth.abi.encodeParameters(['tuple(uint256,uint256,uint256,uint256,uint256)', 'tuple(string)'],[[0, 0, 0, 0, 0], ["test"]])
-        const votingData = {
-          questionId: 2,
-          starterAddress: from,
-          starterGroupId: 0,
-          endTime: 0,
-          data,
-        }
-        
-        await zeroOne.startVoting(votingData);
-        try {
-          await token.approve(zeroOne.address, adminBalance);
-          await zeroOne.setVote(2);
-
-          increase(web3, 320000);
-          await zeroOne.submitVoting();
-          const [event] = await zeroOne.getPastEvents('VotingEnded');
-          const {args: {descision}} = event;
-          assert.strictEqual(descision.toNumber(), 2);
-          console.log(`voting with formula --- ${formula} --- ✔️`);
-        } catch ({message}) {
-          console.log(`test #${formulas.indexOf(formula)} ---  ${message} --- 🚫`)
-        }
-      }
-    });
+    it('should be undefined', async () => {});
   });
+
+  describe("erc20{%s}->conditions{quorum>50%, positive=100% of quorum}", () => {
+    it('should be positive', async () => {});
+
+    it('should be negative', async () => {});
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>0%, positive>50% of all}", () => {
+    it('should be positive', async () => {});
+
+    it('should be negative', async () => {});
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>50%,positive>90% of quorum} or custom{%t}->admin", () => {
+    it('should be positive', async () => {});
+
+    it('should be negative', async () => {});
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>0%,positive>90% of quorum} and custom{%t}->admin", () => {
+    it('should be positive', async () => {});
+
+    it('should be negative', async () => {});
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>30%, positive>50% of all} and (custom{%t}->conditions{quorum>30%, positive>50% of all} and custom{%t}->admin)", () => {
+    it('should be positive', async () => {});
+
+    it('should be negative', async () => {});
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->user{%u}->conditions{quorum>0%, positive>50% of all}", () => {
+    it('should be positive', async () => {});
+
+    it('should be negative', async () => {});
+    
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>50%, positive>90% of quorum}", () => {
+    it('should be positive', async () => {
+
+    });
+
+    it('should be negative', async () => {
+
+    });
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>50%, positive>90% of quorum}", () => {
+    it('should be positive', async () => {
+
+    });
+
+    it('should be negative', async () => {
+
+    });
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>50%, positive>90% of quorum}", () => {
+    it('should be positive', async () => {
+
+    });
+
+    it('should be negative', async () => {
+
+    });
+
+    it('should be undefined', async () => {});
+  });
+
+  describe("erc20{%s}->conditions{quorum>50%, positive>90% of quorum}", () => {
+    it('should be positive', async () => {
+
+    });
+
+    it('should be negative', async () => {
+
+    });
+
+    it('should be undefined', async () => {});
+  });
+
 });
